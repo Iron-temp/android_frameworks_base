d913d4024d6e1fdcf50e0b6f383dee1e7ac23dd5
Hanif Ardhani <hanif.ardhani98@gmail.com>
2021-12-10 11:21:35 +0700

    Merge tag 'android-12.0.0_r16' of https://android.googlesource.com/platform/frameworks/base into twelve
    Android 12.0.0 release 16
    
    * tag 'android-12.0.0_r16' of https://android.googlesource.com/platform/frameworks/base: (604 commits)
      Add config to detect e-call call based on dialed sim emerg # list.
      [RESTRICT AUTOMERGE] Add Wi-Fi Scanning notify to QS Internet Dialog
      Update the BroadcastQueue logic for runtime permissions
      [RESTRICT AUTOMERGE] Add WiFi scan notify message (QS Internet Dialog)
      [DO NOT MERGE] Fix DozeScreenBrightnessTest.
      Continue the pausing process after automatically entering pip
      [DO NOT MERGE] Improve brightness handling during screen off timeout.
      Apply the last recents animation transform on single-activity PIP tasks
      Import translations. DO NOT MERGE ANYWHERE
      Import translations. DO NOT MERGE ANYWHERE
      Camera: Avoid possible deadlock when closing advanced extension session
      Camera: Fix wrong wasImageCaptured flag
      Prevent multiple outstanding permission queries per client
      Revert changes to StationaryThrottlingLocationProvider
      Stop RBC affecting brightness so drastically
      Fix not show translation if call startTranslation after pauseTranslation
      Import translations. DO NOT MERGE ANYWHERE
      Fix bug checking incorrect AppOp
      Update dozing state on clock view attached
      Import translations. DO NOT MERGE ANYWHERE
      ...
    
    Change-Id: Ia17120d9fce1793a623c452250dcb7fa0dffe941
    
    Conflicts:
    	core/java/android/hardware/fingerprint/FingerprintManager.java
    	packages/SystemUI/res/values/dimens.xml
    	packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
    	services/core/java/com/android/server/policy/SingleKeyGestureDetector.java

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -497,6 +497,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     int mLidNavigationAccessibility;
     int mShortPressOnPowerBehavior;
     int mLongPressOnPowerBehavior;
+    long mLongPressOnPowerAssistantTimeoutMs;
     int mVeryLongPressOnPowerBehavior;
     int mDoublePressOnPowerBehavior;
     int mTriplePressOnPowerBehavior;
@@ -793,6 +794,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.Global.getUriFor(
                     Settings.Global.POWER_BUTTON_LONG_PRESS), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Global.getUriFor(
+                    Settings.Global.POWER_BUTTON_LONG_PRESS_DURATION_MS), false, this,
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.Global.getUriFor(
                     Settings.Global.POWER_BUTTON_VERY_LONG_PRESS), false, this,
@@ -1843,6 +1847,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 com.android.internal.R.integer.config_shortPressOnPowerBehavior);
         mLongPressOnPowerBehavior = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_longPressOnPowerBehavior);
+        mLongPressOnPowerAssistantTimeoutMs = mContext.getResources().getInteger(
+                com.android.internal.R.integer.config_longPressOnPowerDurationMs);
         mVeryLongPressOnPowerBehavior = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_veryLongPressOnPowerBehavior);
         mDoublePressOnPowerBehavior = mContext.getResources().getInteger(
@@ -2092,7 +2098,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
      */
     private final class PowerKeyRule extends SingleKeyGestureDetector.SingleKeyRule {
         PowerKeyRule(int gestures) {
-            super(KEYCODE_POWER, gestures);
+            super(mContext, KEYCODE_POWER, gestures);
         }
 
         @Override
@@ -2104,6 +2110,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         void onPress(long downTime) {
             powerPress(downTime, 1 /*count*/,
                     mSingleKeyGestureDetector.beganFromNonInteractive());
+        }
+
+        @Override
+        long getLongPressTimeoutMs() {
+            if (getResolvedLongPressOnPowerBehavior() == LONG_PRESS_POWER_ASSISTANT) {
+                return mLongPressOnPowerAssistantTimeoutMs;
+            } else {
+                return super.getLongPressTimeoutMs();
+            }
         }
 
         @Override
@@ -2158,7 +2173,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
      */
     private final class BackKeyRule extends SingleKeyGestureDetector.SingleKeyRule {
         BackKeyRule(int gestures) {
-            super(KEYCODE_BACK, gestures);
+            super(mContext, KEYCODE_BACK, gestures);
         }
 
         @Override
@@ -2178,7 +2193,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     private void initSingleKeyGestureRules() {
-        mSingleKeyGestureDetector = new SingleKeyGestureDetector(mContext);
+        mSingleKeyGestureDetector = new SingleKeyGestureDetector();
 
         int powerKeyGestures = 0;
         if (hasVeryLongPressOnPowerBehavior()) {
@@ -2296,6 +2311,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     Settings.Global.POWER_BUTTON_LONG_PRESS,
                     mContext.getResources().getInteger(
                             com.android.internal.R.integer.config_longPressOnPowerBehavior));
+            mLongPressOnPowerAssistantTimeoutMs = Settings.Global.getLong(
+                    mContext.getContentResolver(),
+                    Settings.Global.POWER_BUTTON_LONG_PRESS_DURATION_MS,
+                    mContext.getResources().getInteger(
+                            com.android.internal.R.integer.config_longPressOnPowerDurationMs));
             mVeryLongPressOnPowerBehavior = Settings.Global.getInt(resolver,
                     Settings.Global.POWER_BUTTON_VERY_LONG_PRESS,
                     mContext.getResources().getInteger(
@@ -5745,6 +5765,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         pw.print(prefix);
                 pw.print("mLongPressOnPowerBehavior=");
                 pw.println(longPressOnPowerBehaviorToString(mLongPressOnPowerBehavior));
+        pw.print(prefix);
+        pw.print("mLongPressOnPowerAssistantTimeoutMs=");
+        pw.println(mLongPressOnPowerAssistantTimeoutMs);
         pw.print(prefix);
                 pw.print("mVeryLongPressOnPowerBehavior=");
                 pw.println(veryLongPressOnPowerBehaviorToString(mVeryLongPressOnPowerBehavior));
